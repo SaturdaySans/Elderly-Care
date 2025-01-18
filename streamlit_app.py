84cf5e0ff33a97bfac205af6f71219ad718d83e9
Saturday <saturdaysansseven@gmail.com>
2025-01-18 19:52:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Alzheimer Help")
 st.write(
     "Let's tart building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
