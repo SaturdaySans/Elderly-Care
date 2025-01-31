5c872b8e5bb9a4264a5e188b8c90c92655b29af6
Saturday <saturdaysansseven@gmail.com>
2025-02-01 01:44:34 +0800

    k

---
+++ Home.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import os
+import pandas as pd
+
+#Variables
+login_account = None
+accounts = pd.read_csv("accounts.txt") #Assign the data
+routine = pd.read_csv("daily_routine.txt")
+
+#Functions
+def main_menu_UI():
+    st.title("--- Alzheimer help ---")
+    st.divider()
+    if st.button("Schedule"):
+        st.session_state.page = "events"
+
+
+def create_account(accounts):
+    st.write("hi")
+
+def login(accounts):
+    st.write("hi")
+
+def main():
+    main_menu_UI()
+
+
+#Call main
+main()
+
+
+
+
+
+#Todo: 
+#Rewrite validation for daily routine, HHMM format, etc.
+#Refine input validation for account creation, email validation, etc.
+#Set up streamlit UI
+#GPS tracking 
+#Daily routine
+#Reminders
+#Move main menu UI in main() to user_interface()
+#Admin account that can delete accounts?
+#Allow user to go back to main menu in the sub-menus.
+#Add file close() everywhere it is missing.
+#Optimise the code, make it neater.
+#Add a way to go back to the main menu in all sub-menus.
+#Fix delete event
+#Add edit event function
+#Add view events function
