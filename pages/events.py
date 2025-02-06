147100151667d1a3df86c6fd1f6198bf4e70ca97
Tyler, SST <ang_jia_hao_tyler@s2022.ssts.edu.sg>
2025-02-06 09:46:00 +0800

    Broken schedule

--- pages/events.py
+++ pages/events.py
@@ -2,6 +2,52 @@ import streamlit as st
 import os
 import pandas as pd
 
-st.write("hi")
 routine = pd.read_csv("daily_routine.txt")
+
+st.write("--- Event Manager ---")
+if st.button("Create Event"):
+    st.write("Create Event")
+    event_name = st.text_input("Event Name")
+    event_date = st.text_input("Event Date")
+    event_time = st.text_input("Event Time")
+    event_location = st.text_input("Event Location")
+    event_description = st.text_input("Event Description")
+    event = pd.DataFrame({"Event Name": [event_name], "Event Date": [event_date], "Event Time": [event_time], "Event Location": [event_location], "Event Description": [event_description]})
+    #routine = routine.append(event)
+    routine.to_csv("daily_routine.txt", index=False)
+    st.write("Event created successfully.")
+    st.write(routine)
+if st.button("Edit Event"):
+    st.write("Edit Event")
+    event_name = st.text_input("Event Name")
+    event_date = st.text_input("Event Date")
+    event_time = st.text_input("Event Time")
+    event_location = st.text_input("Event Location")
+    event_description = st.text_input("Event Description")
+    event = pd.DataFrame({"Event Name": [event_name], "Event Date": [event_date], "Event Time": [event_time], "Event Location": [event_location], "Event Description": [event_description]})
+    #routine = routine.append(event)
+    routine.to_csv("daily_routine.txt", index=False)
+    st.write("Event edited successfully.")
+    st.write(routine)
+if st.button("Delete Event"):
+    st.write("Delete Event")
+    event_name = st.text_input("Event Name")
+    event_date = st.text_input("Event Date")
+    event_time = st.text_input("Event Time")
+    event_location = st.text_input("Event Location")
+    event_description = st.text_input("Event Description")
+    event = pd.DataFrame({"Event Name": [event_name], "Event Date": [event_date], "Event Time": [event_time], "Event Location": [event_location], "Event Description": [event_description]})
+    #routine = routine.append(event)
+    routine.to_csv("daily_routine.txt", index=False)
+    st.write("Event deleted successfully.")
+    st.write(routine)
+if st.button("View Events"):
+    st.write("View Events")
+    st.write(routine)
+
+
+
+
+
+
 st.write(routine)
