2c25c8ffdfd9b14a2fde39a4bbd81970992d85c3
Saturday <saturdaysansseven@gmail.com>
2025-02-06 09:51:27 +0800

    k

--- pages/schedule.py
+++ pages/schedule.py
@@ -7,4 +7,10 @@ st.divider()
 routine = pd.read_csv("daily_routine.txt")
 st.write(routine)
 if st.button("Edit"):
-    st.write("Test")
+    schedule_name = st.text_input("Event Name:")
+    start_time = st.number_input("Start Time (xxxx):")
+    end_time = st.number_input("End Time (xxxx):")
+    duration = end_time - start_time
+    if st.button("Done!"):
+        dataframe = pd.DataFrame({"Events": [schedule_name], "Start": [start_time], "End": [end_time], "Duration": [duration]})
+        dataframe.to_csv("daily_routine.txt", mode='a', header=False, index=False)
