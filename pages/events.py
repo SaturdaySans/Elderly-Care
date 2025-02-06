74184d32ad9b61234fc354eb0e10d67197023406
Saturday <saturdaysansseven@gmail.com>
2025-02-06 09:39:49 +0800

    Update events.py

--- pages/events.py
+++ pages/events.py
@@ -2,6 +2,9 @@ import streamlit as st
 import os
 import pandas as pd
 
-st.write("hi")
+st.title("Schedule")
+st.divider()
 routine = pd.read_csv("daily_routine.txt")
 st.write(routine)
+if st.button("Edit"):
+    st.write("Test")
