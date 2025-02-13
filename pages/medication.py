9d80471c23dfe3d025c05e8b87cf86696b7cfa20
Saturday <saturdaysansseven@gmail.com>
2025-02-13 09:34:49 +0800

    Update medication.py

--- pages/medication.py
+++ pages/medication.py
@@ -3,3 +3,17 @@ import os
 import pandas as pd
 
 st.title("Medication")
+
+file_path = "medication.csv"
+
+
+if os.path.exists(file_path) and os.stat(file_path).st_size > 0:
+    try:
+        medication = pd.read_csv(file_path, names=["Medication","Time","Taken"], header=None, usecols=[0, 1, 2])
+    except Exception as e:
+        st.error(f"Error reading file: {e}")
+        medication = pd.DataFrame(columns=["Medication","Time","Taken"])
+else:
+    medication = pd.DataFrame(columns=["Medication","Time","Taken"])  # Empty DataFrame
+
+st.write(medication)
