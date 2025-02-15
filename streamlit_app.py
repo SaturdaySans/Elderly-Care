a898d5cc652c6b650a2d24f94643245f2e3095d0
Saturday <saturdaysansseven@gmail.com>
2025-02-16 01:37:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def side_bar_UI():
 
 #Functions
 def main_menu_UI():
-    st.image("banner.png", use_container_width=True)
+    st.image("bannerimage.png", use_container_width=True)
     st.title("--- Alzheimer help ---")
     st.divider()
     side_bar_UI()
