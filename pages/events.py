40ab599f966f69f134b800acdaa83424412a6a43
Saturday <saturdaysansseven@gmail.com>
2025-02-17 12:16:03 +0800

    Update events.py

--- pages/events.py
+++ pages/events.py
@@ -2,10 +2,11 @@ import streamlit as st
 from streamlit_calendar import calendar
 
 def side_bar_UI():
-    st.sidebar.header("Alzheimer Help") #Sets sidebar name to "Alzheimer Help"
+    st.sidebar.header("Alzheimer Help")  # Sets sidebar name to "Alzheimer Help"
     st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
     st.sidebar.page_link("pages/medication.py", label="Medication", icon="💊")
-    st.sidebar.page_link("pages/schedule.py", label="Schedule", icon="📅")
+    st.sidebar.page_link("pages/schedule.py", label="Schedule", icon="🗓️")  
+    st.sidebar.page_link("pages/events.py", label="Events", icon="📆")  
     st.sidebar.page_link("pages/settings.py", label="Settings", icon="⚙️")
 
 side_bar_UI()
