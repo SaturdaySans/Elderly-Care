5ceb000bca07e0e194699229556d1c1047f876db
Saturday <saturdaysansseven@gmail.com>
2025-02-17 12:28:39 +0800

    Update events.py

--- pages/events.py
+++ pages/events.py
@@ -33,7 +33,7 @@ calendar_options = {
     ],
 }
 calendar_events = [
-    {"title": "Event 1", "start": "2023-07-31T08:30:00+08:00", "end": "2023-07-31T10:30:00+08:00", "resourceId": "a"},
+    {"title": "Singing", "start": "2025-02-18T08:30:00+08:00", "end": "2025-002-18T10:30:00+08:00", "resourceId": "a"},
     {"title": "Event 2", "start": "2023-07-31T07:30:00+08:00", "end": "2023-07-31T10:30:00+08:00", "resourceId": "b"},
     {"title": "Event 3", "start": "2023-07-31T10:40:00+08:00", "end": "2023-07-31T12:30:00+08:00", "resourceId": "a"},
 ]
