7588bd4de33e9fd42cdd908a45e99b9efb2fbf6e
Saturday <saturdaysansseven@gmail.com>
2025-02-17 12:27:43 +0800

    Update events.py

--- pages/events.py
+++ pages/events.py
@@ -32,13 +32,16 @@ calendar_options = {
         {"id": "f", "building": "Building C", "title": "Building F"},
     ],
 }
-
+calendar_events = [
+    {"title": "Event 1", "start": "2023-07-31T08:30:00+08:00", "end": "2023-07-31T10:30:00+08:00", "resourceId": "a"},
+    {"title": "Event 2", "start": "2023-07-31T07:30:00+08:00", "end": "2023-07-31T10:30:00+08:00", "resourceId": "b"},
+    {"title": "Event 3", "start": "2023-07-31T10:40:00+08:00", "end": "2023-07-31T12:30:00+08:00", "resourceId": "a"},
+]
 
 
 # Rename the calendar variable to avoid conflict
 calendar_display = calendar(
     events=calendar_events,
-    options=calendar_options,
     key='calendar',
 )
 
