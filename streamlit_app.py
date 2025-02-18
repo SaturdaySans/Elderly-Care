5f4ddb4f051c5d75a15c33c8130ae6a0d73df759
Saturday <saturdaysansseven@gmail.com>
2025-02-18 23:12:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,7 @@ import os
 import pandas as pd
 from st_pages import add_page_title, get_nav_from_toml
 
-# 🛠️ Fix: Move `st.set_page_config()` to the top before any other `st` calls
-st.set_page_config( 
-    layout="wide"
-)
+
 
 nav = get_nav_from_toml(
     ".streamlit/pages_sections.toml" 
