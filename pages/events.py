95bc956d2eea99052a9f9b262d8a228f609aa405
Saturday <saturdaysansseven@gmail.com>
2025-02-18 23:00:42 +0800

    Update events.py

--- pages/events.py
+++ pages/events.py
@@ -2,16 +2,6 @@ import streamlit as st
 from streamlit_calendar import calendar
 import pandas as pd
 
-def side_bar_UI():
-    st.sidebar.header("Alzheimer Help")  # Sets sidebar name to "Alzheimer Help"
-    st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
-    st.sidebar.page_link("pages/medication.py", label="Medication", icon="💊")
-    st.sidebar.page_link("pages/routine.py", label="Routine", icon="🗓️")  
-    st.sidebar.page_link("pages/events.py", label="Events", icon="📆")  
-    st.sidebar.page_link("pages/settings.py", label="Settings", icon="⚙️")
-
-side_bar_UI()
-
 # Read the event data from the CSV file
 file_path = "events.csv"
 
