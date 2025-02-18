846a80f2ff9c2669398a37da35614865ef2ab1a1
Saturday <saturdaysansseven@gmail.com>
2025-02-18 22:18:21 +0800

    k

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import pandas as pd
+from st_pages import Page, show_pages, add_page_title
 
 #Page Name
 st.set_page_config(
@@ -9,6 +10,19 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="auto")
 
+
+
+# Optional -- adds the title and icon to the current page
+add_page_title()
+
+# Specify what pages should be shown in the sidebar, and what their titles 
+# and icons should be
+show_pages(
+    [
+        Page("streamlit_app.py", "Home", "🏠"),
+        Page("pages/events.py", "Page 2", ":books:"),
+    ]
+)
 
 def side_bar_UI():
     st.sidebar.header("Alzheimer Help")  # Sets sidebar name to "Alzheimer Help"
@@ -30,30 +44,6 @@ def main_menu_UI():
     st.page_link("pages/routine.py", label="Routine", icon="🗓️")
     st.page_link("pages/events.py", label="Events", icon="📆")  
     st.page_link("pages/medication.py", label="Medication", icon="💊")
-    """# Row 1
-    col1, col2 = st.columns([1, 2]) 
-    with col1:
-        st.image("resources/settings.png", width=200)
-    with col2:
-        st.page_link("pages/settings.py", label="Settings", icon="⚙️")
-    
-    st.write("")  
-
-    # Row 2
-    col3, col4 = st.columns([2, 1])
-    with col3:
-        st.page_link("pages/routine.py", label="Routine", icon="📅")
-    with col4:
-        st.image("resources/calendar.png", width=200)
-
-    st.write("")  
-
-    #Row 3
-    col5, col6 = st.columns([1, 2])
-    with col5:
-        st.image("resources/medicine.png", width=200)
-    with col6:
-        st.page_link("pages/medication.py", label="Medication", icon="💊")"""
 
 def main():
     main_menu_UI()
