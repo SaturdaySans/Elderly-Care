f01a49683a9a02e6b141e5c939495f5fb7639e7b
Saturday <saturdaysansseven@gmail.com>
2025-02-19 00:22:02 +0800

    Update menu.py

--- pages/menu.py
+++ pages/menu.py
@@ -2,16 +2,10 @@ import streamlit as st
 import os
 import pandas as pd
 
-def main_menu_UI():
-    #st.image("resources/banner.png", use_container_width=True)
-    st.title("--- Alzheimer help ---")
-    st.divider()
-    st.page_link("pages/settings.py", label="Settings", icon="⚙️")
-    st.page_link("pages/routine.py", label="Routine", icon="🗓️")
-    st.page_link("pages/events.py", label="Events", icon="📆")  
-    st.page_link("pages/medication.py", label="Medication", icon="💊")
 
-def main():
-    main_menu_UI()
-
-st.write("test")
+st.title("--- Alzheimer help ---")
+st.divider()
+st.page_link("pages/settings.py", label="Settings", icon="⚙️")
+st.page_link("pages/routine.py", label="Routine", icon="🗓️")
+st.page_link("pages/events.py", label="Events", icon="📆")  
+st.page_link("pages/medication.py", label="Medication", icon="💊")
