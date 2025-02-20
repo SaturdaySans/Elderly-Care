3a5688b79ace6352e547e881cd262a0b3177003d
Saturday <saturdaysansseven@gmail.com>
2025-02-20 11:21:05 +0800

    Update menu.py

--- pages/menu.py
+++ pages/menu.py
@@ -5,7 +5,6 @@ from st_pages import add_page_title, get_nav_from_toml
 from utils import setup_navigation  # Import the global function
 
 pg = setup_navigation()  # Call the function to setup navigation
-pg.run()
 
 st.title("--- Elderly Care ---")
 st.divider()
