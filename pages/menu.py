8ab8f515edb4e67ac27056d6c61f2401223c5cc4
Saturday <saturdaysansseven@gmail.com>
2025-02-20 11:07:40 +0800

    Update menu.py

--- pages/menu.py
+++ pages/menu.py
@@ -10,3 +10,38 @@ st.page_link("pages/settings.py", label="Settings", icon="⚙️")
 st.page_link("pages/routine.py", label="Routine", icon="🗓️")
 st.page_link("pages/events.py", label="Events", icon="📆")  
 st.page_link("pages/medication.py", label="Medication", icon="💊")
+
+
+# Load navigation pages
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+
+# Display app logo
+st.logo("resources/calendar.png")
+
+# Ensure session state role exists
+if "role" not in st.session_state:
+    st.session_state.role = None
+
+ROLES = [None, "User", "Admin"]
+
+if "UID" not in st.session_state:
+    st.session_state["UID"] = [1]
+
+
+if st.session_state["UID"] and st.session_state["UID"][-1] == 0:
+    st.session_state.role = "Admin"
+    st.rerun()
+
+role = st.session_state.role
+
+# Define Admin page
+admin_pages = st.Page("pages/admin.py", title="Admin", icon=":material/security:")
+
+# Merge Admin page into navigation if user is Admin
+if role == "Admin":
+    pg = st.navigation({**nav, "Admin": admin_pages})
+else:
+    pg = st.navigation(nav)
+
+# Run the navigation system
+pg.run()
