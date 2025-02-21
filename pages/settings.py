d180f80b964e0e1d6d5bf56c85e4559b1ab0a95c
Saturday <saturdaysansseven@gmail.com>
2025-02-21 21:14:01 +0800

    Update settings.py

--- pages/settings.py
+++ pages/settings.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import pandas as pd
+from st_pages import hide_pages
 
 # File to store account data
 ACCOUNTS_FILE = "accounts.csv"
@@ -47,6 +48,10 @@ def login():
             st.session_state["username"] = user.iloc[0]["username"]
             st.session_state["UID"] = user.iloc[0]["UID"]  # Set UID after login
             st.session_state["role"] = user.iloc[0]["role"]
+
+            # Check role and hide pages if not admin
+            if st.session_state["role"] != "Admin":
+                hide_pages(["admin_page"])  # Replace "admin_page" with actual page names that should be hidden for users.
 
             st.session_state["page"] = "settings"
             st.rerun()
