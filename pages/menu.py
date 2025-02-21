fce212cd03d6a3fd547fb1a73dec5cbbf5ca9052
Tyler, SST <ang_jia_hao_tyler@s2022.ssts.edu.sg>
2025-02-21 21:49:49 +0800

    updated comments

--- pages/menu.py
+++ pages/menu.py
@@ -2,10 +2,10 @@ import streamlit as st
 import os
 import pandas as pd
 from st_pages import add_page_title, get_nav_from_toml
-from utils import update_navigation  # Import function
+from utils import update_navigation  # Import the function to update the navigation
 
 
-st.title("--- Elderly Care ---")
+st.title("--- Elderly Care ---") #Main menu
 st.divider()
 st.page_link("pages/settings.py", label="Settings", icon="⚙️")
 st.page_link("pages/routine.py", label="Routine", icon="🗓️")
--- pages/routine.py
+++ pages/routine.py
@@ -1,3 +1,4 @@
+#Yong Hong
 import streamlit as st
 import os
 import pandas as pd
