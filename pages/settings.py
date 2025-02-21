fc880afe4e7a482aa0492d6738ad64fa86bcbf9f
Tyler, SST <ang_jia_hao_tyler@s2022.ssts.edu.sg>
2025-02-21 08:30:31 +0800

    fixed setings

--- pages/settings.py
+++ pages/settings.py
@@ -3,10 +3,7 @@ import os
 import pandas as pd
 import random
 from st_pages import get_nav_from_toml, hide_pages
-# settings.py
-import streamlit as st
-import os
-import pandas as pd
+
 
 # File to store account data
 ACCOUNTS_FILE = "accounts.csv"
